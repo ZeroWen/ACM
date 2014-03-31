9e7034496107359eb79f91d024661d10a11ebb14
Jing0 <j.kuo2012@gmail.com>
2014-03-31 23:56:11 +0800

    1-1-7

---
+++ hdu/1-1-7.c
@@ -0,0 +1,42 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  1-1-7.c
+ *
+ *    Description:  
+ *    Problem Description
+ *    Your task is to Calculate a + b.
+ *
+ *    Input
+ *    The input will consist of a series of pairs of integers a and b, separated by a space, one pair of integers per line. 
+ *
+ *    Output
+ *    For each pair of input integers a and b you should output the sum of a and b, and followed by a blank line. 
+ *
+ *    Sample Input
+ *    1 5
+ *    10 20
+ *
+ *    Sample Output
+ *    6
+ *
+ *    30
+ *    
+ *
+ *        Version:  1.0
+ *        Created:  03/31/2014 22:55:40
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jackie Kuo (http://jackiekuo.com), j.kuo2012@gmail.com
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+int main(){
+	int a,b;
+	while(scanf("%d%d",&a,&b)!=EOF)
+		printf("%d\n\n",a+b);
+	return 0;
+}
