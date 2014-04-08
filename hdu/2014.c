8d70f25dcac1a814a6d83a759ccdba5add0b475d
Jing0 <j.kuo2012@gmail.com>
2014-04-09 00:02:26 +0800

    2014

---
+++ hdu/2014.c
@@ -0,0 +1,57 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  2014.c
+ *
+ *    Description:  
+ *    Problem Description
+ *    青年歌手大奖赛中，评委会给参赛选手打分。选手得分规则为去掉一个最高分和一个最低分，然后计算平均得分，请编程输出某选手的得分。
+ *    Input
+ *    输入数据有多组，每组占一行，每行的第一个数是n(2<n<=100)，表示评委的人数，然后是n个评委的打分。
+ *    
+ *    Output
+ *    对于每组输入数据，输出选手的得分，结果保留2位小数，每组输出占一行。
+ *
+ *    Sample Input
+ *    3 99 98 97
+ *    4 100 99 98 97
+ *
+ *    Sample Output
+ *    98.00
+ *    98.50
+ *
+ *        Version:  1.0
+ *        Created:  04/08/2014 23:05:41
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jackie Kuo (http://jackiekuo.com), j.kuo2012@gmail.com
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+int main(){
+	int n,s[100],i,j,t;
+	double sum;
+	while(scanf("%d",&n)!=EOF){
+		for(i=0;i<n;i++)
+			scanf("%d", &a[i]);
+		for(i=0;i<n-1;i++)
+			for(j=i+1;j<n;j++){
+				if(s[i]>s[j]){
+					t=s[i];
+					s[i]=s[j];
+					s[j]=t;
+				}
+			}
+		for(i=1,sum=0;i<n-1;i++)
+			sum+=a[i];
+		printf("%.2lf\n", sum/(n-2));            
+	}
+	return 0;
+}
+
+/*
+ * Note:先排序，去头去尾即可
+ */
