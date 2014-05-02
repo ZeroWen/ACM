764d4210567286d5d5d0d948b3867e955b4aecd3
Jing0 <j.kuo2012@gmail.com>
2014-05-02 14:36:13 +0800

    pom try

--- poj/1000.c
+++ poj/1000.c
@@ -5,28 +5,20 @@
  *
  *    Description:  
  * Calculate a+b
- *    
  * Input
- *
  * Two integer a,b (0<=a,b<=10)
  * Output
- *
  * Output a+b
  * Sample Input
- *
  * 1 2
  * Sample Output
- *
  * 3
  *    
  *
- *        Version:  1.0
  *        Created:  03/30/2014 22:35:34
- *       Revision:  none
  *       Compiler:  gcc
  *
  *         Author:  Jackie Kuo (http://jackiekuo.com), j.kuo2012@gmail.com
- *
  * =====================================================================================
  */
 
