118866647cfe3c0916bab7fe04945c5cfffafae5
Jackie Kuo <j.kuo2012@gmail.com>
2015-03-04 14:17:58 +0800

    1047

--- hdu/1047.c
+++ hdu/1047.c
@@ -4,14 +4,30 @@
  *       Filename:  1047.c
  *
  *    Description:  
+ *    One of the first users of BIT's new supercomputer was Chip Diller. He extended his exploration of powers of 3 to go from 0 to 333 and he explored taking various sums of those numbers.
+ *    ``This supercomputer is great,'' remarked Chip. ``I only wish Timothy were here to see these results.'' (Chip moved to a new apartment, once one became available on the third floor of the Lemon Sky apartments on Third Street.)
+ *    Input
+ *    The input will consist of at most 100 lines of text, each of which contains a single VeryLongInteger. Each VeryLongInteger will be 100 or fewer characters in length, and will only contain digits (no VeryLongInteger will be negative).
+ *    The final input line will contain a single zero on a line by itself.
+ *    Output
+ *    Your program should output the sum of the VeryLongIntegers given in the input.
+ *    This problem contains multiple test cases!
+ *    The first line of a multiple input is an integer N, then a blank line followed by N input blocks. Each input block is in the format indicated in the problem description. There is a blank line between input blocks.
+ *    The output format consists of N output blocks. There is a blank line between output blocks.
+ *    Sample Input
+ *    1
  *
- *        Version:  1.0
+ *    123456789012345678901234567890
+ *    123456789012345678901234567890
+ *    123456789012345678901234567890
+ *    0
+ *    Sample Output
+ *    370370367037037036703703703670
+ *    
  *        Created:  02/24/2015 13:50:40
- *       Revision:  none
  *       Compiler:  gcc
  *
  *         Author:  Jackie Kuo (http://jackiekuo.com), j.kuo2012@gmail.com
- *   Organization:  
  *
  * =====================================================================================
  */
